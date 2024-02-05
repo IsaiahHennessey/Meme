730d8179b44cf41b82f7a95852e5ed0650fd64d1
IsaiahHennessey <153872012+IsaiahHennessey@users.noreply.github.com>
2024-02-04 18:26:58 -0800

    Update Meme.js

--- Meme.js
+++ Meme.js
@@ -1,54 +1,26 @@
-const imageFileInput = document.querySelector('#imageFileInput');
-const topTextInput = document.querySelector('#topTextInput');
-const bottomTextInput = document.querySelector('#bottomTextInput');
-const canvas = document.querySelector('#meme');
+function generateMeme() {
+    const imageUrl = document.getElementById('imageUrl').value.trim();
+    const bottomTextInput = document.getElementById('bottomTextInput').value.trim();
+    const topTextInput = document.getElementById('topTextInput').value.trim();
 
-let image;
+    const memeContainer = document.createElement('div');
+    memeContainer.classList.add('meme');
 
-imageFileInput.addEventListener('change', () => {
-const imageDataUrl = URL.createObjectURL(imageFileInput.files[0]);
+    const image = document.createElement('img');
+    image.src = imageUrl;
 
-image = new Image();
-image.src = imageDataUrl;
+    const topText = document.createElement('div');
+    topText.classList.add('TopText');
+    topText.textContent = topTextInput;
 
-image.addEventListener('load', () => {
-    updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
-}, { once: true });
-});
+    const bottomText = document.createElement('div');
+    bottomText.classList.add('BottomText');
+    bottomText.textContent = bottomTextInput;
 
-topTextInput.addEventListener('change', () => {
-    updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
-});
+    memeContainer.appendChild(image);
+    memeContainer.appendChild(topText);
+    memeContainer.appendChild(bottomText);
 
-bottomTextInput.addEventListener('change', () => {
-    updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
-});
-
-
-function updateMemeCanvas(canvas, image, topText, bottomText) {
-    const context = canvas.getContext('2d');
-    const width = image.width;
-    const height = image.height;
-    const fontSize = Math.floor(width / 7);
-    const yOffset = height / 25;
-
-    canvas.width = width;
-    canvas.height = height;
-    context.drawImage(image, 0, 0)
-
-
-    context.strokeStyle = 'black';
-    context.lineWidth = Math.floor(fontSize / 3);
-    context.fillStyle = 'white';
-    context.textAlign = 'center';
-    context.lineJoin = 'round';
-    context.font = `${fontSize}px sans-serif`;
-
-    context.textBaseline = 'top';
-    context.strokeText(topText, width / 2, yOffset);
-    context.fillText(topText, width / 2, yOffset);
-
-    context.textBaseline = 'bottom';
-    context.strokeText(bottomText, width / 2, height - yOffset);
-    context.fillText(bottomText, width / 2, height - yOffset);
+    const divArea = document.getElementById('divArea');
+    divArea.appendChild(memeContainer);
 }
